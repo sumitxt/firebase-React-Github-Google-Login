9d8fa012ad7c5c52adcd53c9dd7181ddaeb458b2
Sumit Barua <sumitbarua14@gmail.com>
2022-12-06 16:15:34 +0600

    functionalComponents

--- src/components/home/HomeBanner.js
+++ src/components/home/HomeBanner.js
@@ -1,6 +1,43 @@
-import React from 'react';
+import React, {useEffect, useRef, useState} from 'react';
+import axios from "axios";
 
 const HomeBanner = (props) => {
+    const [color,setColor]=useState('red')
+
+    const NewColor=()=>{
+        setColor('Blue')
+    }
+    const NewGreen=()=>{
+        setColor('Green')
+    }
+
+    let UserName=useRef();
+    let Password=useRef();
+
+    const SubmitForm=()=>{
+        let a=UserName.current.value;
+        let b=Password.current.value;
+        alert(a)
+        alert(b)
+    }
+
+    let [todo,SetTodo]=useState("")
+
+    //use effect
+
+    useEffect(()=>{
+        axios.get('https://jsonplaceholder.typicode.com/todos').then((res)=>{
+            setTimeout(()=>{
+                SetTodo(res.data)
+            },3000)
+
+        }).catch((error)=>{
+
+        })
+        }
+
+    )
+
     return (
         <div>
             <h1>Sumit Barua</h1>
@@ -8,6 +45,15 @@ const HomeBanner = (props) => {
             <h1>{props.subtitle}</h1>
             <h2>{props.subject.name}</h2>
             <h2>{props.subject.age}</h2>
+
+            <h1>{color}</h1>
+            <button onClick={NewColor}>Change</button>
+            <button onClick={NewGreen}>Change Green</button>
+            <br/>
+            <input ref={UserName} placeholder="UserName" type="text" />
+            <input ref={Password} placeholder="UserPassword" type="text" />
+            <button onClick={SubmitForm}>Submit</button>
+            <p>{JSON.stringify(todo)}</p>
         </div>
     );
 };
